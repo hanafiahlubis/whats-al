6819fd881f9141cab5aef5c789cd7fcd2bf07ee7
Ali Hanafiah <hanafiaha308@gmail.com>
2023-04-07 00:24:58 +0700

    reposife profil

--- public/assets/java-script/login/index-daftar.js
+++ public/assets/java-script/login/index-daftar.js
@@ -5,11 +5,13 @@ document.kirim.onsubmit = async (e) => {
     data.append("password", document.kirim.password.value);
     data.append("name", document.kirim.nama.value);
     data.append("file", document.kirim.file.files[0]);
-
+    document.querySelector("#daftar-button button").disabled = false;
     const response = await fetch("/api/akun", {
         method: "POST",
         body: data,
     });
+
+    document.querySelector("#daftar-button button").disabled = true;
     alert(await response.text());
     if (response.ok) location.href = "/login";
 };
